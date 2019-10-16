d655720064f1ca1286cd85a55475a86b3ec01558
jsonp <jason@kiind.design>
2019-10-16 03:13:57 +0000

    add ale to vim

--- vim/install.sh
+++ vim/install.sh
@@ -6,9 +6,16 @@ pkg_mgr_install vim
 rm -f $HOME/.vimrc
 ln -s $DOTFILES/vim/config.symlink ~/.vimrc
 
-# symlink colors
+rm -rf $HOME/.vim
+mkdir -p $HOME/.vim/pack/typescript/start
+mkdir -p $HOME/.vim/pack/git-plugins/start
+
 echo "SYMLINKING $DOTFILES/vim/src/colors to $HOME/.vim/colors"
-rm -rf $HOME/.vim/colors
-mkdir $HOME/.vim
 ln -s $DOTFILES/vim/src/colors $HOME/.vim/colors
+
+echo "SYMLINKING $DOTFILES/vim/src/typescript-vim to $HOME/.vim/pack/typescript/start/typescript-vim"
+ln -s $DOTFILES/vim/src/typescript-vim $HOME/.vim/pack/typescript/start/typescript-vim
+
+echo "SYMLINKING $DOTFILES/vim/src/ale to $HOME/.vim/pack/git-plugins/start/ale"
+ln -s $DOTFILES/vim/src/ale $HOME/.vim/pack/git-plugins/start/ale
 
