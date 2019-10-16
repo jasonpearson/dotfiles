a82c6bbdad1a106fd429bb5cfe6dc9752dfe1d5a
jsonp <jason@kiind.design>
2019-10-16 03:24:11 +0000

    add vim auto-pairs

--- vim/install.sh
+++ vim/install.sh
@@ -7,8 +7,7 @@ rm -f $HOME/.vimrc
 ln -s $DOTFILES/vim/config.symlink ~/.vimrc
 
 rm -rf $HOME/.vim
-mkdir -p $HOME/.vim/pack/typescript/start
-mkdir -p $HOME/.vim/pack/git-plugins/start
+mkdir -p $HOME/.vim/{pack,plugin}/{typescript,git-plugins}/start
 
 echo "SYMLINKING $DOTFILES/vim/src/colors to $HOME/.vim/colors"
 ln -s $DOTFILES/vim/src/colors $HOME/.vim/colors
@@ -18,4 +17,7 @@ ln -s $DOTFILES/vim/src/typescript-vim $HOME/.vim/pack/typescript/start/typescri
 
 echo "SYMLINKING $DOTFILES/vim/src/ale to $HOME/.vim/pack/git-plugins/start/ale"
 ln -s $DOTFILES/vim/src/ale $HOME/.vim/pack/git-plugins/start/ale
+
+echo "SYMLINKING $DOTFILES/vim/src/auto-pairs/plugins/auto-pairs.vim to $HOME/.vim/plugin/auto-pairs.vim"
+ln -s $DOTFILES/vim/src/auto-pairs/plugin/auto-pairs.vim $HOME/.vim/plugin/
 
