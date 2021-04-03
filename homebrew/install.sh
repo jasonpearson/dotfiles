f68c59d2a9bac3868df989249313869345478ff9
Jason Pearson <jsonp@Jasons-MacBook-Pro.local>
2021-04-03 12:40:45 -0700

    various

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,7 +2,7 @@
 if ! command -v brew &> /dev/null
 then
   echo "RUNNING homebrew install script"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   xargs brew install < $DOTFILES/homebrew/homebrew.txt
 else
   echo "SKIPPING homebrew (already installed)"
