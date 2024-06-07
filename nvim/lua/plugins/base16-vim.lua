94b21f8d89f35d2e25953abde63ca5c26a1dc3c2
jsonp <jsonp@hey.com>
2024-06-07 08:40:02 -0700

    update

--- nvim/lua/plugins/base16-vim.lua
+++ nvim/lua/plugins/base16-vim.lua
@@ -2,6 +2,5 @@ return {
 	"tinted-theming/base16-vim",
 	config = function()
 		vim.cmd("colorscheme base16-" .. os.getenv("BASE16_THEME"))
-		vim.api.nvim_command("hi CursorLine ctermbg=19")
 	end,
 }
