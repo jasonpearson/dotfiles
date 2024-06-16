c97946b6a8fcf4e7056de52eb732669f0ddce080
jsonp <jsonp@hey.com>
2024-06-15 18:18:26 -0700

    fix highlighting

--- nvim/lua/plugins/base16-vim.lua
+++ nvim/lua/plugins/base16-vim.lua
@@ -2,5 +2,8 @@ return {
 	"tinted-theming/base16-vim",
 	config = function()
 		vim.cmd("colorscheme base16-default-dark")
+		vim.api.nvim_set_hl(0, "Visual", { ctermbg = 141, ctermfg = 236 })
+		vim.api.nvim_set_hl(0, "TelescopeSelection", { ctermbg = 141, ctermfg = 236 })
+		vim.api.nvim_set_hl(0, "TelescopeMatching", { ctermfg = 2 })
 	end,
 }
